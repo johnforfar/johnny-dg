734ad9c3e22c54571186e0c95f352ab82bf74152
johnforfar <46335740+johnforfar@users.noreply.github.com>
2025-10-16 11:31:11 +0800

    more nix fixes

--- example/flake.nix
+++ example/flake.nix
@@ -1,7 +1,7 @@
 {
   inputs = {
     xnode-manager.url = "github:Openmesh-Network/xnode-manager";
-    johnny-dg.url = "github:johnforfar/johnny-dg"; # "path:..";
+    johnny-dg.url = "github:johnforfar/johnny-dg?ref=main"; # "path:..";
     nixpkgs.follows = "johnny-dg/nixpkgs";
   };
 
